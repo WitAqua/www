2900c1a468ab4b1a403e807dee2a583097d25f74
neroices <git@slce.moe>
2024-12-20 06:39:25 +0700

    www: Add favicon
    Change-Id: Ie71b320b58407aa60143c370fc9a873171d0f8d7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
 import { ThemeProvider } from "./providers";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
@@ -14,6 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body
         className={`${inter.className} min-h-screen bg-background antialiased`}
       >
@@ -26,9 +27,7 @@ export default function RootLayout({
           <div className="relative flex min-h-screen flex-col">
             <SiteHeader />
 
-            <main className="flex-1">
-              {children}
-            </main>
+            <main className="flex-1">{children}</main>
 
             <SiteFooter />
           </div>
