544281aa3e9204e5337ac15850e863df49431c77
neroices <git@slce.moe>
2024-12-20 13:46:19 +0700

    www: Update about page
    Change-Id: I4caa377519e4a76058d38f511cb27d881a3a1f41

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,13 +8,19 @@ const teamMembers = [
     name: "Toufu",
     role: "Founder/Lead Developer",
     image: "/toufu.png",
-    github: "toufune"
+    github: "toufune",
   },
   {
     name: "Maitani-Sakura",
     role: "Co-Founder/Developer",
     image: "/maitani-sakura.png",
-    github: "maitani-sakura"
+    github: "maitani-sakura",
+  },
+  {
+    name: "soralis0912",
+    role: "Developer",
+    image: "/soralis0912.png",
+    github: "soralis0912",
   },
   {
     name: "neroices",
@@ -26,21 +32,26 @@ const teamMembers = [
     name: "MONE-FIERA",
     role: "Developer",
     image: "/monefiera.png",
-    github: "monefiera"
+    github: "monefiera",
   },
   {
     name: "Yumagi",
     role: "Developer",
     image: "/yumagi.png",
-    github: "ymag-h"
- },
- {
-  name: "satokun2668",
-  role: "Designer",
-  image: "/satokun.jpg",
-  github: "numaaqours"
-}
-
+    github: "ymag-h",
+  },
+  {
+    name: "satokun2668",
+    role: "Designer",
+    image: "/satokun.jpg",
+    github: "numaaqours",
+  },
+  {
+    name: "Garry050",
+    role: "Advisor",
+    image: "/garry050.png",
+    github: "garry050",
+  },
 ];
 
 const fadeInUp = {
@@ -60,18 +71,27 @@ export default function AboutPage() {
         animate: { opacity: 1, transition: { staggerChildren: 0.1 } },
       }}
     >
-      <motion.h1 className="text-4xl font-bold mb-8 text-center" variants={fadeInUp}>
+      <motion.h1
+        className="text-4xl font-bold mb-8 text-center"
+        variants={fadeInUp}
+      >
         About WitAqua
       </motion.h1>
       <motion.section className="mb-12 text-left" variants={fadeInUp}>
         <p className="text-lg mb-4">
-        {"We're a small team of passionate Android enthusiasts from Japan, and we've come together to create something special. It all started because we love the simplicity of stock Android but felt it could use a little more personality and practicality without all the unnecessary bloat. So, we rolled up our sleeves and got to work, crafting a clean, and responsive."}
+          {
+            "We're a small team of passionate Android enthusiasts from Japan, and we've come together to create something special. It all started because we love the simplicity of stock Android but felt it could use a little more personality and practicality without all the unnecessary bloat. So, we rolled up our sleeves and got to work, crafting a clean, and responsive."
+          }
         </p>
         <p className="text-lg mb-4">
-        {"We kept the core Android experience intact while adding some carefully chosen enhancements to make your device more customizable, and just plain better to use. We're not a big corporation or a fancy tech giant. We're just a group of like-minded developers who love tinkering with Android and making it better for everyone."}
+          {
+            "We kept the core Android experience intact while adding some carefully chosen enhancements to make your device more customizable, and just plain better to use. We're not a big corporation or a fancy tech giant. We're just a group of like-minded developers who love tinkering with Android and making it better for everyone."
+          }
         </p>
         <p className="text-lg">
-        {"So, whether you're here to try something new, or just curious about what we're building, welcome to WitAqua! We're excited to have you join us on this journey at a time."}
+          {
+            "So, whether you're here to try something new, or just curious about what we're building, welcome to WitAqua! We're excited to have you join us on this journey at a time."
+          }
         </p>
       </motion.section>
       <motion.section variants={fadeInUp}>
